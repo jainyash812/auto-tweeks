7871df50797fe562c6ed092ad6812d840224a17b
Yash Jain <yash.jain@augmentolabs.com>
2023-07-02 18:57:33 +0530

    features section and favicon icon added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LandingSection from './sections/landing/LandingSection'
 import Navbar from './components/Navbar'
 import NewArrivals from './sections/new-arrivals/NewArrivals'
 import BestSeller from './sections/best-seller/BestSeller'
+import Features from './sections/features/Features'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <LandingSection/>
       <NewArrivals/>
       <BestSeller />
+      <Features />
     </>
   )
 }
---
+++ src/sections/features/Features.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Features.css';
+const Features = () => {
+  return (
+    <div className='features-section-container'>
+        <div className='features-section-item'>
+            <h5>Free Delivery</h5>
+            <p>Lorem Ipsum temp sum fun eleipend,hdgp aioa euoiw wuywuyw </p>
+        </div>
+        <div className='features-section-item'>
+            <h5>Money Guarantee</h5>
+            <p>Lorem Ipsum temp sum fun eleipend</p>
+        </div>
+        <div className='features-section-item'>
+            <h5>Online Support</h5>
+            <p>Lorem Ipsum temp sum fun eleipend</p>
+        </div>
+    </div>
+  )
+}
+
+export default Features
